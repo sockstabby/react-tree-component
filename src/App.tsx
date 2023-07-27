4cbaa97676f0bb7ce95b092189090ca39e3561a3
Your Name <you@example.com>
2023-07-27 18:34:27 -0500

    fixed compile issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,11 @@
-import { useState } from "React";
-
 import "./App.css";
 
 import Widget from "./Widget";
-
 import Search from "./Search";
-
-interface Item {
-  image: string;
-}
-
-const myFunction2 = (padding: "left" | "center" | "right") => {
-  console.log(padding);
-};
-
-const wrapperFunction = (fn: (items: [Item] | null) => void, items: Item[]) => {
-  return fn(items);
-};
-
-const myFunction = (items: Item[] | null) => {
-  if (items != null) {
-    return items[0].image;
-  } else {
-    return null;
-  }
-};
 
 function App() {
   const width = 100;
   const height = 100;
-
-  const [ item, setItem ] = useState()
-
-  const images: Item[] = [{ image: "adsfasdf" }];
-
-  //let r = myFunction2("whatevers");
-
-  let val = wrapperFunction(myFunction, images);
-
-  //let ret = myFunction(images);
 
   return (
     <>
